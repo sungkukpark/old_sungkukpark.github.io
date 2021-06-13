41947044ceb6724717addbeabec43ab272abf1b7
Sungkuk Park <sungkuk.park@wooga.net>
2021-06-13 06:03:50 +0200

    Install disquis

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,25 +2,25 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-// import { Disqus, CommentCount } from 'gatsby-plugin-disqus'
+import { DiscussionEmbed } from "disqus-react"
 
 const BlogPost = ({ data }) => {
   const post = data.markdownRemark
-  // let disqusConfig = {
-  //   // TODO: Add url and identifier to Disquis config object
-  //   // url: `${config.siteUrl+location.pathname}`,
-  //   // identifier: post.frontmatter.identifier,
-  //   title: post.frontmatter.title,
-  // }
+  const disqusConfig = {
+    shortname: process.env.GATSBY_DISQUS_NAME,
+    config: {
+      identifier: post.frontmatter.identifier,
+      title: post.frontmatter.title
+    },
+  }  
   return (
     <Layout>
       <SEO title={post.frontmatter.title} description={post.excerpt} />
       <div>
         <h1>{post.frontmatter.title}</h1>
-        {/* <CommentCount config={disqusConfig} placeholder={'...'} /> */}
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        {/* <Disqus config={disqusConfig} /> */}
-      </div>
+        <DiscussionEmbed {...disqusConfig} />
+DiscussionEmbed      </div>
     </Layout>
   )
 }
