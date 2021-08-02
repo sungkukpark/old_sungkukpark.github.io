8c4f22912658d127bcff325f74136ee8795550c2
Sungkuk Park <sungkuk.park@wooga.net>
2021-08-02 08:13:14 +0200

    Fix naming convention warning

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Seo from "../components/seo"
 import { DiscussionEmbed } from "disqus-react"
 
 const BlogPost = ({ data }) => {
@@ -15,7 +15,7 @@ const BlogPost = ({ data }) => {
   }  
   return (
     <Layout>
-      <SEO title={post.frontmatter.title} description={post.excerpt} />
+      <Seo title={post.frontmatter.title} description={post.excerpt} />
       <div>
         <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
