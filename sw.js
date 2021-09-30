f13c0b51fcf1da89cf54800f5d474210e5a3bec2
Sungkuk Park <sungkuk.park@wooga.net>
2021-09-30 22:48:36 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-74606d7e637039b6f6bf.js"
+    "url": "webpack-runtime-4a92e7797309c65dc866.js"
   },
   {
     "url": "framework-a17c9893df41f16dd6ed.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "00ad7590d4961469c7d8dc936b28aa8b"
+    "revision": "891e478ae09b900e41b29d032334b6c5"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b3411e3bcf9659fa3cfa.js"
