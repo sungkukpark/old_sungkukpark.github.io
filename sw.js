455ecfce315c48629cc23d1bb6d2e307b8b9a4b7
Sungkuk Park <sungkuk.park@wooga.net>
2022-06-21 12:56:33 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-588b2c29673bcafe46f6.js"
+    "url": "webpack-runtime-8252d574152bad24945e.js"
   },
   {
     "url": "framework-a17c9893df41f16dd6ed.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "26ad5e19fdf908778cc283f576a6278d"
+    "revision": "24d8c31519b52065f9c556c689f7a1d6"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b3411e3bcf9659fa3cfa.js"
